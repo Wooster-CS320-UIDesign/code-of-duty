adc58934e1506c1886a7a00ef6092d82ff64d002
Huy Kieu <hkieu22@wooster.edu>
2020-10-22 06:26:22 +0700

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Code of Duty
+        </p>
+        <a
+          className="App-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Let's begin the project!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
